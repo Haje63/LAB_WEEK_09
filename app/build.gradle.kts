b9d8eceb55d57eaabd368670485629467463fdf9
Haje63 <hyunjunglol63@gmail.com>
2025-11-06 23:58:02 +0700

    Commit No. 3 – UI Element and Theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.foundation)
+    implementation(libs.androidx.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
